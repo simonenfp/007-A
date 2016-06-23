3d3fa0a2675f81456cd2d98bdaa374ccef31fef4
simonenfp <qudajiangtaihe@163.com>
2016-06-23 18:26:14 +0800

    research status bar

--- app/src/main/java/com/simonenfp/me/activity/MainActivity.java
+++ app/src/main/java/com/simonenfp/me/activity/MainActivity.java
@@ -1,13 +1,21 @@
 package com.simonenfp.me.activity;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.flyco.tablayout.SlidingTabLayout;
 import com.flyco.tablayout.widget.MsgView;
@@ -18,6 +26,7 @@ import com.simonenfp.me.fragment.SecondFragment;
 import com.simonenfp.me.fragment.ThirdFragment;
 import com.simonenfp.me.mvp.presenter.MainActivityPresenter;
 import com.simonenfp.me.mvp.view.MainActivityView;
+import com.simonenfp.me.utils.StatusBarTools;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,11 +50,52 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View viewRoot = findViewById(R.id.linear1);
+        Log.d("DDDD","viewRoot:"+viewRoot.getId());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+//            StatusBarTools.dealStatusBar(this,true);
+            setTransparentStatusBar();
+        }
         mListTabTitles = new ArrayList<>();
         mListFragments = new ArrayList<>();
         initView();
         //presenter get data
         mPresenter.getTabData();
+    }
+
+    private void setTransparentStatusBar() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
+        View statusBarView = mContentView.getChildAt(0);
+        Log.d("DDDD","statusBarView:"+statusBarView.getId());
+        //移除假的 View
+        if (statusBarView != null && statusBarView.getLayoutParams() != null && statusBarView.getLayoutParams().height == getStatusBarHeight(this)) {
+            mContentView.removeView(statusBarView);
+        }
+        View vi = mContentView.getChildAt(0);
+        if (vi == null){
+            Log.d("DDDD","vi == null");
+        }else {
+            Log.d("DDDD","vi != null"+" "+vi.getId());
+        }
+//        //不预留空间
+//        if (mContentView.getChildAt(0) != null) {
+//            ViewCompat.setFitsSystemWindows(mContentView.getChildAt(0), false);
+//        }
+
+
+
+    }
+    //Get status bar height
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) {
+            result = context.getResources().getDimensionPixelOffset(resId);
+        }
+        return result;
     }
 
     private void initView() {

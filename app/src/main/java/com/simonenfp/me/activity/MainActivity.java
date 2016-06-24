3a7f931db5f05d5c35f5c4f631dfbb64408ff10e
simonenfp <qudajiangtaihe@163.com>
2016-06-24 10:07:48 +0800

    modify bug

--- app/src/main/java/com/simonenfp/me/activity/MainActivity.java
+++ app/src/main/java/com/simonenfp/me/activity/MainActivity.java
@@ -77,6 +77,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
 
         ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
         View rootView = mContentView.getChildAt(0);
+        //
+        //
 
         if (rootView != null){
 //            LinearLayoutCompat.LayoutParams layoutParams = new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);

39ecaec9930636d0cc9172d36850ff8c087d284a
simonenfp <simonenfp@163.com>
2016-06-25 22:15:08 +0800

    transparent status to support LOLLIPOP

--- app/src/main/java/com/simonenfp/me/utils/StatusBarTools.java
+++ app/src/main/java/com/simonenfp/me/utils/StatusBarTools.java
@@ -12,6 +12,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
+import com.simonenfp.me.R;
+
 /**
  * Created by simonenfp on 2016/6/23.
  */
@@ -19,18 +21,48 @@ public class StatusBarTools {
 
     public static void dealStatusBar(Activity activity){
         //set transparent status after 4.4
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
             Window window = activity.getWindow();
-            //set transparent status
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
+
+            //set transparent status and let main layout extend to status
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //set transparent navigation and let main layout extend to navigation
+//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//
             ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
 
             //set contentView rootView padding in order to set aside the location of the power strip
             View rootView = mContentView.getChildAt(0);
             if (rootView != null){
+//                rootView.setFitsSystemWindows(true);
                 rootView.setPadding(0,getStatusBarHeight(activity),0,0);
+            }
+        }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+
+
+            Window window = activity.getWindow();
+
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    //Let the main layout extend to navigation
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            //
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            //set navigation transparent
+//            window.setNavigationBarColor(Color.TRANSPARENT);
+
+
+
+            ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+//                ViewCompat.setFitsSystemWindows(mChildView, true);
+                mChildView.setPadding(0,getStatusBarHeight(activity),0,0);
             }
         }
     }

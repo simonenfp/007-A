dfb03e8bed998f952c9eb54d7ed0af021eeb6828
simonenfp <qudajiangtaihe@163.com>
2016-08-26 18:00:01 +0800

    custom view ----volume

--- app/src/main/java/com/simonenfp/me/module/ui/activity/CustomViewExample1Activity.java
+++ app/src/main/java/com/simonenfp/me/module/ui/activity/CustomViewExample1Activity.java
@@ -6,7 +6,6 @@ import android.widget.ScrollView;
 
 import com.simonenfp.me.R;
 import com.simonenfp.me.base.BaseActivity;
-import com.simonenfp.me.widget.MyScrollView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,19 +13,13 @@ import butterknife.OnClick;
 
 public class CustomViewExample1Activity extends BaseActivity {
 
-    @Bind(R.id.scrollView)
-    MyScrollView scrollView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view_example1);
-        ButterKnife.bind(this);
     }
 
-    public void onScrollTv(View view) {
-        scrollView.smoothScrollTo(300,400);
-    }
+
 
 
 }
---
+++ app/src/main/java/com/simonenfp/me/widget/CustomVolume.java
@@ -0,0 +1,83 @@
+package com.simonenfp.me.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Scroller;
+
+/**
+ * Created by simonenfp on 2016/8/26.
+ */
+public class CustomVolume extends View {
+    Paint mPaint;
+    RectF mRectF;
+    int mCircleWidth;
+    public CustomVolume(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+        mRectF = new RectF();
+        mCircleWidth = 30;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int radius = 100;
+        int rx = radius + mCircleWidth/2 + 300;
+        int ry = radius + mCircleWidth/2 + 600;
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.argb(0x33,0x5f,0x2c,0xe9));
+        mPaint.setStrokeWidth(mCircleWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(rx,ry,radius,mPaint);
+
+        //绘制圆弧
+        mPaint.setColor(Color.argb(0xff,0x43,0x93,0x12));
+        mPaint.setStrokeWidth(mCircleWidth-10);
+        mRectF = new RectF(rx - radius,ry - radius,rx + radius,ry + radius);
+        canvas.drawArc(mRectF,0,arc,false,mPaint);
+
+        //绘制百分比
+        mPaint.reset();
+        mPaint.setColor(Color.argb(0x88,0x43,0x93,0x12));
+        mPaint.setTextSize(40);
+        String s = String.valueOf(progress) + "%";
+        Rect rect = new Rect();
+        mPaint.getTextBounds(s,0,s.length(),rect);
+        canvas.drawText(s,rx - rect.width()/2,ry + rect.height()/2,mPaint);
+
+
+    }
+
+    private int lastY;
+    private int arc;
+    private int progress;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaY = (y - lastY)/2;
+                if (deltaY >= 0){
+                    arc = arc - deltaY > 0 ? arc - deltaY : 0;
+
+                }else {
+                    arc = -deltaY + arc > 360 ? 360 : -deltaY + arc;
+                }
+                progress = arc*100/360 ;
+                postInvalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        lastY = y;
+        return true;
+    }
+}

34474660fda261e6b8d1a0bd6770514a72dfa96e
simonenfp <qudajiangtaihe@163.com>
2016-11-11 18:35:07 +0800

    animator

--- app/src/main/java/com/simonenfp/me/module/ui/activity/AnimatorActivity.java
+++ app/src/main/java/com/simonenfp/me/module/ui/activity/AnimatorActivity.java
@@ -1,10 +1,12 @@
 package com.simonenfp.me.module.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.simonenfp.me.R;
 import com.simonenfp.me.base.BaseActivity;
 import com.simonenfp.me.loading.AnimationView;
+import com.simonenfp.me.loading.FuckProgressBar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,12 +16,17 @@ public class AnimatorActivity extends BaseActivity {
 
     @Bind(R.id.av)
     AnimationView av;
+    @Bind(R.id.progress)
+    ImageView progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animator);
         ButterKnife.bind(this);
+        FuckProgressBar fuckProgressBar = new FuckProgressBar(this);
+        progress.setImageDrawable(fuckProgressBar);
+        fuckProgressBar.start();
 
     }
 
